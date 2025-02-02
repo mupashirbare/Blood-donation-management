f4b2b09e25c62f47be585ca95ead03cbd462707f
mupashirbare <mubashirbarre12@gmail.com>
2025-02-01 22:05:31 -0800

    i have added Authentication Middleware.

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateUser = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid Token" });
+  }
+};
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access Denied" });
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizeRoles };
