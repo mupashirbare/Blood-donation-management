ac158795cb10d7938d930d1b9d9a7a7b32c0c710
mupashirbare <mubashirbarre12@gmail.com>
2025-02-14 06:35:19 -0800

    i was apdated the bloodRequest table on sidebar.

--- frontend/src/pages/BloodRequests.jsx
+++ frontend/src/pages/BloodRequests.jsx
@@ -43,8 +43,6 @@ const BloodRequests = () => {
 
   return (
     <div className="flex">
-      {/* Sidebar */}
-      <Sidebar />
 
       {/* Main Content */}
       <div className="flex-1 p-6 bg-gray-100 min-h-screen">
